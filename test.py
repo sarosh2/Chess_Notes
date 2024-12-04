5a87de744629442cd4f182b4ae3282f18a9855e5
sarosh2 <saroshanish@gmail.com>
2024-12-04 03:04:09 +0200

    Removed Test File

--- test.py
+++
@@ -1,8 +0,0 @@
-import chess
-import chess.polyglot
-
-board = chess.Board()
-
-with chess.polyglot.open_reader("openings\Human-polyglot\Human.bin") as reader:
-   for entry in reader.find_all(board):
-       print(entry.move, entry.weight, entry.learn)
